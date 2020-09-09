f74dd4365b0ee2dbb37f0f3211ee762017e71c32
JOSEFFE  Oliveira <OLIVEJ14@sa.ad.whirlpool.com>
2020-09-09 20:16:07 -0300

    Backend com SelectAll e SelectDetail

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,4 +4,7 @@ const router = express.Router();
 
 router.post('/usuarios', UsuarioController.Insert);
 
+router.get('/usuarios', UsuarioController.SelectAll);
+router.get('/usuarios/:id', UsuarioController.SelectDetail);
+
 module.exports = router;
